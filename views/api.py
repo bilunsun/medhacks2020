15f3f22b22ec167ea2101427502ab2681e776d22
Mahad Janjua <mahad.j15@hotmail.com>
2020-09-06 01:28:17 -0400

    Fully working, just need to finish text parts

--- views/api.py
+++ views/api.py
@@ -1,5 +1,13 @@
 import flask
 from flask import Flask, request, render_template
+import sys
+sys.path.append("..\medhacks2020")
+from web_utils import get_single_inference_class
+from model import GeneClassifier
+import torch
+
+model = GeneClassifier()
+model.load_state_dict(torch.load("model.pth"))
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
@@ -14,7 +22,9 @@ def classify():
     text = str(request.form.getlist('text')[0])
     gene = str(request.form.getlist('gene')[0])
     mutation = str(request.form.getlist('mutation')[0])
-    classification = text
-    return render_template("index.html", classification=classification)
+    classification = get_single_inference_class(text, gene, mutation, model)
+    classification = classification.index(max(classification)) + 1
+    filler_response = "Your classification is: " + str(classification) + "!"
+    return render_template("index.html", classification=filler_response)
 
 app.run()
--- web_utils.py
+++ web_utils.py
@@ -1,10 +1,12 @@
 from data_utils import get_single_inference_ids
+import torch
 
 
 def get_single_inference_class(text, gene, mutation, model):
     model.eval()
 
     ids = get_single_inference_ids(text, gene, mutation)
-    prediction = model(ids)
+    ids = torch.LongTensor(ids).view(1, -1)
+    prediction = model(ids).flatten()
 
-    return prediction.to_list()
+    return prediction.tolist()
